42ffb394c7b498f5054b148a09275627d298b1b7
Evandro Junior <47798920+evandrmb@users.noreply.github.com>
2021-08-22 00:44:06 -0300

    Created WriteButton.test.js

---
+++ src/components/WriteButton/WriteButton.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import WriteButton from "./WriteButton";
+
+describe("WriteButton", () => {
+  test("Should be a button", () => {
+    render(<WriteButton />);
+
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  test("Should execute onClick", async () => {
+    let number = 0;
+    const incrementNumber = () => number++;
+
+    render(<WriteButton onClick={incrementNumber} />);
+
+    const button = await screen.findByRole("button");
+
+    fireEvent.click(button);
+    expect(number).toBe(1);
+  });
+
+  test("Should find 'Write' label in button", () => {
+    render(<WriteButton />);
+
+    expect(screen.getByText("Write")).toBeInTheDocument();
+  });
+
+  test("Should find image", () => {
+    render(<WriteButton />);
+
+    expect(screen.getByRole("img")).toBeInTheDocument();
+  });
+
+  test("Should find add.svg as image src", () => {
+    render(<WriteButton />);
+
+    expect(screen.getByAltText("add").src).toBe("http://localhost/add.svg");
+  });
+});
