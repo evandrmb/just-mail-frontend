8853fa120e2f8180681ccfc03d0c9d41a24d2dcf
Evandro Junior <47798920+evandrmb@users.noreply.github.com>
2021-10-11 23:40:26 -0300

    feat: created Button

---
+++ src/components/Button/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: var(--royal-blue);
+  border: none;
+  border-radius: 0.25rem;
+  color: var(--white);
+  cursor: pointer;
+  font-weight: 700;
+  height: 2.5rem;
+  outline: none;
+`;
+
+export default StyledButton;
