1419c1bad326bc741d9d8e5f55756fc6cfbf229e
Evandro Junior <47798920+evandrmb@users.noreply.github.com>
2021-10-11 23:53:01 -0300

    feat: created SignUpPage for mobile devices

---
+++ src/pages/SignUp/styled.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const StyledSignUpView = styled.div`
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 1.5rem 1rem;
+`;
+
+export const StyledTitle = styled.h1`
+  font-weight: 700;
+  font-size: 1.875rem;
+  margin-bottom: 1.5rem;
+`;
+
+export const StyledAuthButtonBar = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const StyledCheckboxContainer = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+`;
