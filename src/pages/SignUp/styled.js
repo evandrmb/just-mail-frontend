a7c5beff9be538841b7fe12cb967b77562be18d6
Evandro Junior <47798920+evandrmb@users.noreply.github.com>
2021-10-13 01:21:02 -0300

    feat: created components to style SignUpPage when displayed in medium to large devices

--- src/pages/SignUp/styled.js
+++ src/pages/SignUp/styled.js
@@ -1,11 +1,48 @@
 import styled from "styled-components";
 
 export const StyledSignUpView = styled.div`
-  align-items: flex-start;
+  align-items: center;
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
+  height: 100vh;
+  justify-content: center;
   padding: 1.5rem 1rem;
+  width: 100vw;
+
+  @media (min-width: 720px) {
+    align-items: center;
+    justify-content: center;
+    padding: 5rem;
+  }
+`;
+
+export const StyledSignUpForm = styled.form`
+  @media (min-width: 720px) {
+    border: 1px solid var(--mako);
+    border-radius: 1rem;
+    padding: 1.5rem 2rem;
+    width: 100%;
+  }
+`;
+
+export const StyledFirstNameGridArea = styled.div`
+  grid-area: firstName;
+`;
+export const StyledLastNameGridArea = styled.div`
+  grid-area: lastName;
+`;
+export const StyledUsernameGridArea = styled.div`
+  grid-area: username;
+`;
+
+export const StyledNameDiv = styled.div`
+  @media (min-width: 720px) {
+    display: grid;
+    column-gap: 1rem;
+    grid-template-areas:
+      "firstName  lastName"
+      "username username";
+  }
 `;
 
 export const StyledTitle = styled.h1`
